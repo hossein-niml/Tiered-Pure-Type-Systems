9772f4de798b35391c680aa32ffc991086884912
Hossein Madadi <hossein.niml@gmail.com>
2026-06-26 03:56:02 +0330

    add alpha-eq in PTS.v

--- codes/PTS.v
+++ codes/PTS.v
@@ -55,4 +55,57 @@ Fixpoint dom (c : context) : list var :=
   | (x, _) :: c' => x :: dom c'
   end.
 
+Fixpoint rename (x y : var) (t : term) : term :=
+  match t with
+  | t_sort s => t_sort s
+  | t_var z => if eq_var_dec z x then t_var y else t_var z
+  | t_pi z A B =>
+      if eq_var_dec z x
+      then t_pi z (rename x y A) B
+      else t_pi z (rename x y A) (rename x y B)
+  | t_lam z A B =>
+      if eq_var_dec z x
+      then t_lam z (rename x y A) B
+      else t_lam z (rename x y A) (rename x y B)
+  | t_app M N => t_app (rename x y M) (rename x y N)
+  end.
+
+Reserved Notation "t1 =a t2" (at level 70, no associativity).
+
+Inductive alpha_eq : term -> term -> Prop :=
+
+  | alpha_sort : forall s, t_sort s =a t_sort s
+
+  | alpha_var : forall x, t_var x =a t_var x
+
+  | alpha_app : forall M1 M2 N1 N2,
+      M1 =a M2 -> N1 =a N2 -> t_app M1 N1 =a t_app M2 N2
+
+  | alpha_prod_same : forall x A1 B1 A2 B2,
+      (A1 =a A2) -> (B1 =a B2) -> ((t_pi x A1 B1) =a (t_pi x A2 B2))
+      
+  | alpha_prod_diff : forall x y A1 B1 A2 B2,
+      ~ In y (fv (t_pi x A1 B1)) ->
+      A1 =a A2 ->
+      B1 =a (rename x y B2) ->
+      t_pi x A1 B1 =a t_pi y A2 B2
+
+  | alpha_lam_same : forall x A1 B1 A2 B2,
+      A1 =a A2 ->
+      B1 =a B2 ->
+      t_lam x A1 B1 =a t_lam x A2 B2
+
+  | alpha_lam_diff : forall x y A1 B1 A2 B2,
+      ~ In y (fv (t_lam x A1 B1)) ->
+      A1 =a A2 ->
+      B1 =a (rename x y B2 ) ->
+      t_lam x A1 B1 =a t_lam y A2 B2
+
+where "t1 =a t2" := (alpha_eq t1 t2).
+
+Lemma alpha_refl : forall t, t =a t.
+Proof.
+  induction t; constructor; auto.
+Qed.
+
 End PTS.
